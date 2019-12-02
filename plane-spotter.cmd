d693d486bfc8e22501d1acfee1fa3b54909f0362
Patrick Coleman <blinken@gmail.com>
2019-12-02 11:44:04 -0800

    add PCB design
    fit a GPS receiver, compass, LED ring and supercapacitor on top of a
    raspberry pi A+ form factor (plus a little bit)

---
+++ plane-spotter.cmd
@@ -0,0 +1,77 @@
+# Pin name action command file
+
+# Start of element COMPASS1
+ChangePinName(COMPASS1, 8, 8)
+ChangePinName(COMPASS1, 7, 7)
+ChangePinName(COMPASS1, 6, 6)
+ChangePinName(COMPASS1, 5, 5)
+ChangePinName(COMPASS1, 4, 4)
+ChangePinName(COMPASS1, 3, 3)
+ChangePinName(COMPASS1, 2, 2)
+ChangePinName(COMPASS1, 1, 1)
+
+# Start of element PIXEL1
+ChangePinName(PIXEL1, 6, 6)
+ChangePinName(PIXEL1, 5, 5)
+ChangePinName(PIXEL1, 4, 4)
+ChangePinName(PIXEL1, 3, 3)
+ChangePinName(PIXEL1, 2, 2)
+ChangePinName(PIXEL1, 1, 1)
+
+# Start of element C1
+ChangePinName(C1, 2, -)
+ChangePinName(C1, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, -)
+ChangePinName(C3, 1, +)
+
+# Start of element GPS1
+ChangePinName(GPS1, 12, 12)
+ChangePinName(GPS1, 11, 11)
+ChangePinName(GPS1, 10, 10)
+ChangePinName(GPS1, 9, 9)
+ChangePinName(GPS1, 8, 8)
+ChangePinName(GPS1, 7, 7)
+ChangePinName(GPS1, 6, 6)
+ChangePinName(GPS1, 5, 5)
+ChangePinName(GPS1, 4, 4)
+ChangePinName(GPS1, 3, 3)
+ChangePinName(GPS1, 2, 2)
+ChangePinName(GPS1, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 26, SPI_CE1_N)
+ChangePinName(U1, 25, GND)
+ChangePinName(U1, 24, SPI_CE0_N)
+ChangePinName(U1, 23, SPI_SCLK)
+ChangePinName(U1, 22, GPIO25)
+ChangePinName(U1, 21, SPI_MISO)
+ChangePinName(U1, 20, GND)
+ChangePinName(U1, 19, SPI_MOSI)
+ChangePinName(U1, 18, GPIO24)
+ChangePinName(U1, 17, 3V3)
+ChangePinName(U1, 16, GPIO23)
+ChangePinName(U1, 15, GPIO22)
+ChangePinName(U1, 14, GND)
+ChangePinName(U1, 13, GPIO27)
+ChangePinName(U1, 12, GPIO18)
+ChangePinName(U1, 11, GPIO17)
+ChangePinName(U1, 10, RXD0)
+ChangePinName(U1, 9, GND)
+ChangePinName(U1, 8, TXD0)
+ChangePinName(U1, 7, GPIO_GCLK)
+ChangePinName(U1, 6, GND)
+ChangePinName(U1, 5, SCL1)
+ChangePinName(U1, 4, 5V0)
+ChangePinName(U1, 3, SDA1)
+ChangePinName(U1, 2, 5V0)
+ChangePinName(U1, 1, 3V3)
