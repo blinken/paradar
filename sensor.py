9d2a63b81da563c62a1430944b512236ecbbac0d
Patrick Coleman <blinken@gmail.com>
2019-12-16 21:27:32 +1100

    tracking, refactor into class

--- sensor.py
+++ sensor.py
@@ -62,6 +62,8 @@ class Compass:
     self._write(_REG_TMRC, 0x98)
     self._write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71)
 
+    self.update()
+
   def _debug(self):
     print("Data ready: " + str(GPIO.input(self._GPIO_DRDY)))
     print("Clearing _REG_POLL & reg_cmm")
@@ -135,15 +137,19 @@ class Compass:
     return (x, y, z)
 
   def get_azimuth(self):
-    x, y, z = self.get_raw_measurements()
-    return (180/math.pi * math.atan2(y, x)) % 360
+    return self._azimuth
 
-  def wait_for_data(self):
+  def update(self):
     while GPIO.input(self._GPIO_DRDY) == 0:
       time.sleep(_INSTRUCTION_SLEEP) # todo, convert to wait_for_edge
 
+    x, y, z = self.get_raw_measurements()
+    self._azimuth = (180/math.pi * math.atan2(y, x)) % 360
+
+    return self._azimuth
+
   def to_string(self):
-    return str(round(self.get_azimuth(), 1))
+    return str(round(self._azimuth, 1))
 
 class Display:
   _GPIO_DATA = board.D18
@@ -157,87 +163,93 @@ class Display:
   def __init__(self):
     self.pixels = neopixel.NeoPixel(board.D18, _PIXEL_COUNT, auto_write=False)
     self.off()
+    self._refresh()
+
+  def _refresh(self):
     self.pixels.show()
 
-  def off(self):
-    for i in range(self._PIXEL_COUNT):
-      self.pixels[i] = (0, 0, 0)
-
-  def calculate_pixel(self, bearing):
+  def _pixel_for_bearing(self, bearing):
     uncorrected_pixel = (self._PIXEL_COUNT - 1) - int(bearing/self._DEGREES_PER_PIXEL)
 
     return (uncorrected_pixel + self.PIXEL_ANGLE_OFFSET) % (self._PIXEL_COUNT - 1)
 
-  def update_aircraft_display(self, compass_angle):
-    self.off()
-
-    # Show compass north in blue
-    self.pixels[calculate_pixel(compass_angle)] = _COLOUR_COMPASS_NORTH
-
-    for value in list(aircraft_list.values()):
-      ac_bearing, ac_distance = calculate_bearing(value, compass_angle)
-      if ac_distance > 100000.0:
-        # squelch aircraft too distant
-        continue
-
-      next_pixel = calculate_pixel(ac_bearing)
-      next_brightness = int(max(0, min(255, ((15000-ac_distance)*255/10000.0))))
-
-      self.pixels[next_pixel] = (next_brightness, 10, 00)
-
-    self.pixels.show()
-
-  def calculate_bearing(ac_value, compass_angle):
+  def _calculate_bearing(ac_value, compass_angle):
     current_lat, current_lon = (0.0, 0.0) # placeholder, needs to be current location
 
     result = Geodesic.WGS84.Inverse(current_lat, current_lon, ac_value[1], ac_value[2])
     bearing = ((result['azi1']+180) + compass_angle) % 360
     return (bearing, result['s12'])
 
+  def off(self):
+    for i in range(self._PIXEL_COUNT):
+      self.pixels[i] = (0, 0, 0)
 
-aircraft_list = {}
+  # Refresh the display with the value of self.pixels
+  def update(self, compass, aircraft_list):
+    self.off()
 
-def track_aircraft(aircraft_list):
-  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  s.connect(("127.0.0.1", 30003))
-  while True:
-    line = s.makefile(buffering=1024000).readline().strip().split(',')
-    #print(line)
-    if line[0] != "MSG" or line[1] != "3":
-      continue
+    # Indicate compass north
+    self.pixels[self._pixel_for_bearing(compass.get_azimuth())] = _COLOUR_COMPASS_NORTH
 
-    # ['MSG', '3', '1', '1', '3C66B3', '1', '2019/11/26', '16:37:53.908', '2019/11/26', '16:37:53.950', '', '8650', '', '', '51.64426', '0.10422', '', '', '', '', '', '0\n']
-    ac_id = line[4]
-    ac_date = line[6]
-    ac_time = line[7]
-    ac_lat = line[14]
-    ac_lon = line[15]
+    for value in list(aircraft_list.values()):
+      ac_bearing, ac_distance = self._calculate_bearing(value, compass.get_azimuth())
+      if ac_distance > 100000.0:
+        # squelch aircraft too distant
+        continue
 
+      next_pixel = self._pixel_for_bearing(ac_bearing)
+      next_brightness = int(max(0, min(255, ((15000-ac_distance)*255/10000.0))))
+
+      self.pixels[next_pixel] = (next_brightness, 10, 00)
+
+    self._refresh()
+
+class Aircraft:
+  positions = {}
+
+  def __init__(self):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.sock.connect(("127.0.0.1", 30003))
+
+  def _parse(self, line):
     try:
+      # ['MSG', '3', '1', '1', '3C66B3', '1', '2019/11/26', '16:37:53.908', '2019/11/26', '16:37:53.950', '', '8650', '', '', '51.64426', '0.10422', '', '', '', '', '', '0\n']
+      (msg_class, msg_type, _, _, ac_id, _, ac_date, ac_time, _, _, _, _, _, _, ac_lat, ac_lon, _) = line.split(",", 16)
+
+      if msg_class != "MSG" or msg_type != "3":
+        continue
+
       ac_datetime = datetime.strptime(ac_date + " " + ac_time, "%Y/%m/%d %H:%M:%S.%f")
-      aircraft_list[ac_id] = (ac_datetime, float(ac_lat), float(ac_lon))
+      ac_lat = float(ac_lat)
+      ac_lon = float(ac_lon)
     except ValueError:
       print("Bad coordinate string: '{}'".format(line))
-      continue
+      return None
 
-    ac_bearing, ac_distance = calculate_bearing(aircraft_list[ac_id], 0)
-    update_time = aircraft_list[ac_id][0].isoformat()
-    print("Updating aircraft {}: {:<7,}meters  {:<5}° {}".format(ac_id, int(ac_distance), round(ac_bearing, 1), update_time))
+    return {ac_id: (ac_datetime, ac_lat, ac_lon)}
 
-    # Clean up values older than 300 seconds
-    for ac_id, value in aircraft_list.copy().items():
-      if (datetime.now() - value[0]) > timedelta(minutes=1):
-        print("Removing expired aircraft " + ac_id)
-        del aircraft_list[ac_id]
+  def track_aircraft(self):
+    while True:
+      ac_result = self._parse(self.sock.makefile(buffering=1024000).readline().strip())
 
-t_ac = threading.Thread(target=track_aircraft, args=(aircraft_list, ), daemon=True)
+      if ac_result == None:
+        continue
+
+      Aircraft.positions.update(ac_result)
+
+      # Clean up values older than 300 seconds
+      for ac_id, value in Aircraft.positions.copy().items():
+        if (datetime.now() - value[0]) > timedelta(minutes=1):
+          print("Removing expired aircraft " + ac_id)
+          del Aircraft.positions[ac_id]
+
+ac = Aircraft()
+t_ac = threading.Thread(target=ac.track_aircraft, args=(ac, ), daemon=True)
 t_ac.start()
 
-
-c = Compass()
+compass = Compass()
+display = Display()
 
 while True:
-  c.wait_for_data()
-
-  compass_angle = round(c.get_azimuth(), 1)
-  update_aircraft_display(compass_angle)
+  compass.update() # wait for a new compass reading and update internal state
+  display.update(compass, Aircraft.positions)
