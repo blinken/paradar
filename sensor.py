ea6d3516037789b9793123c9706d0a080e6989e4
Patrick Coleman <blinken@gmail.com>
2019-12-16 17:54:52 +1100

    compass, refactor into class
    Refactor the compass logic into a class so I can begin to separate this
    out from future GPS/display logic

--- sensor.py
+++ sensor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import spidev
+from spidev import SpiDev
 import time
 import struct
 import math
@@ -14,17 +14,120 @@ from datetime import datetime, timedelta
 
 import board
 import neopixel
+
+GPIO.setmode(GPIO.BCM)
+
+# Clean up GPIOs on exit
+def signal_handler(sig, frame):
+  GPIO.cleanup()
+  sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+class Compass:
+  _REG_POLL = 0x00
+  _REG_CONTINUOUS_MEASUREMENT_MODE = 0x01
+  _REG_CYCLE_COUNT_X_H = 0x04
+  _REG_CYCLE_COUNT_X_L = 0x05
+  _REG_TMRC = 0x0b
+
+  def __init__(self):
+    GPIO.setup(24, GPIO.OUT) # Compass chip select
+    GPIO.setup(23, GPIO.IN)  # Compass DRDY
+    GPIO.output(24, GPIO.HIGH)
+
+    self.spi = SpiDev()
+    self.spi.open(0, 0)
+    self.spi.max_speed_hz = 1000000
+    self.spi.cshigh = False
+
+    # Clear REG_POLL & reg_cmm
+    _write(_REG_POLL, 0x00)
+    _write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x00)
+
+    # Set cycle count
+    _set_cycle_count()
+
+    # Activate continuous measurement
+    _write(_REG_TMRC, 0x98)
+    _write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71)
+
+  def _debug(self):
+    print("Data ready: " + str(GPIO.input(23)))
+    print("Clearing _REG_POLL & reg_cmm")
+    print("_REG_POLL: " + hex(_write(_REG_POLL, 0x00) & 0x80))
+    print("reg_cmm: " + hex(_write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x00) & 0x80))
+
+    print("Setting cycle count")
+    _set_cycle_count()
+
+    print("Setting up continuous measurement")
+    print("_REG_TMRC: " + hex(_write(_REG_TMRC, 0x98) & 0x80))
+    print("reg_cmm: " + hex(_write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71) & 0x80))
+
+  # Wrap a method that does something with the chip, activating the chip-select
+  # before and after it is accessed.
+  def chip_select(self, function):
+    def decorator():
+      time.sleep(0.03)
+      GPIO.output(24, GPIO.LOW)
+      time.sleep(0.03)
+
+      result = funtion()
+
+      time.sleep(0.03)
+      GPIO.output(24, GPIO.HIGH)
+      time.sleep(0.03)
+
+      return result
+
+    return decorator
+
+  @chip_select
+  def _write(self, reg, value):
+    return self.spi.xfer2([reg, value])[0]
+
+  @chip_select
+  def _read(self, reg):
+    return self.spi.xfer2([reg | 0x80, 0x00])[1]
+
+  @chip_select
+  def _set_cycle_count(self):
+    self.spi.xfer2([_REG_CYCLE_COUNT_X_H, 0x00, 0xc8, 0x00, 0xc8, 0x00, 0xc8])
+    #self.spi.xfer2([_REG_CYCLE_COUNT_X_H, 0x01, 0x90, 0x01, 0x90, 0x01, 0x90])
+
+  def _unpack_measurement(self, l):
+    x = bytearray(l + [0x00])
+    return round((struct.unpack(">i", x)[0] >>1)/46603.0, 2)
+
+  @chip_select
+  def get_raw_measurements(self):
+    # From the design guide --
+    # Normally it is only necessary to send "A4", since the register value automatically
+    # increments on the clock cycles such that after sending "A4" all 3 bytes for the X axis
+    # measurement would be clocked out, then the 3 bytes for the Y axis measurement, then the 3
+    # bytes for the Z axis measurement. After these 9 bytes have been clocked out, the subsequent
+    # output data has no relevance.
+    self.spi.xfer2([0xa4])[0]
+    results = self.spi.readbytes(9)
+    return (_unpack_measurement(results[:3]), _unpack_measurement(results[3:6]), _unpack_measurement(results[6:9]))
+
+  def get_azimuth(self):
+    x, y, z = get_raw_measurements()
+    return round((180/math.pi * math.atan2(y, x)) % 360, 1)
+
+  def wait_for_data(self):
+    while GPIO.input(23) == 0:
+      time.sleep(0.02)
+
+  def to_string(self):
+    return str(get_azimuth())
+
 pixels = neopixel.NeoPixel(board.D18, 24, auto_write=False)
 
 for i in range(24):
   pixels[i] = (0, 0, 0)
 
 pixels.show()
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT) # Compass chip select
-GPIO.setup(23, GPIO.IN)  # Compass DRDY
-GPIO.output(24, GPIO.HIGH)
 
 aircraft_list = {}
 
@@ -64,26 +167,6 @@ def track_aircraft(aircraft_list):
 t_ac = threading.Thread(target=track_aircraft, args=(aircraft_list, ), daemon=True)
 t_ac.start()
 
-# Clean up GPIOs on exit
-def signal_handler(sig, frame):
-  GPIO.cleanup()
-  sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
-print("Set up strip.")
-
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 1000000
-spi.cshigh = False
-
-reg_poll = 0x00
-reg_continuous_measurement_mode = 0x01
-reg_cycle_count_x_h = 0x04
-reg_cycle_count_x_l = 0x05
-reg_status = 0x34
-reg_measurement_x_2 = 0x24
-reg_tmrc = 0x0b
 
 def calculate_pixel(bearing):
   degrees_per_pixel = 360/24.0
@@ -122,72 +205,10 @@ def calculate_bearing(ac_value, compass_angle):
   bearing = ((result['azi1']+180) + compass_angle) % 360
   return (bearing, result['s12'])
 
-def cs(state):
-  time.sleep(0.03)
-  if state:
-    GPIO.output(24, GPIO.LOW)
-  else:
-    GPIO.output(24, GPIO.HIGH)
-  time.sleep(0.03)
-
-def write(reg, value):
-  cs(True)
-  res = spi.xfer2([reg, value])[0]
-  cs(False)
-  return res
-
-def read(reg):
-  cs(True)
-  res = spi.xfer2([reg | 0x80, 0x00])[1]
-  cs(False)
-  return res
-
-def get_drdy():
-  return read(reg_status) & 0x80
-
-def unpack_result(l):
-  x = bytearray(l + [0x00])
-  res = round((struct.unpack(">i", x)[0] >>1)/46603.0, 2)
-  return res
-
-def get_compass_results():
-  # Normally it is only necessary to send "A4", since the register value automatically
-  # increments on the clock cycles such that after sending "A4" all 3 bytes for the X axis
-  # measurement would be clocked out, then the 3 bytes for the Y axis measurement, then the 3
-  # bytes for the Z axis measurement. After these 9 bytes have been clocked out, the subsequent
-  # output data has no relevance.
-  cs(True)
-  spi.xfer2([0xa4])[0]
-  results = spi.readbytes(9)
-  cs(False)
-  return (unpack_result(results[:3]), unpack_result(results[3:6]), unpack_result(results[6:9]))
-
-def calculate_compass_azimuth(x, y, z):
-  return round((180/math.pi * math.atan2(y, x)) % 360, 1)
-
-print("Data ready: " + str(GPIO.input(23)))
-print("Clearing reg_poll & reg_cmm")
-print("reg_poll: " + hex(write(reg_poll, 0x00) & 0x80))
-print("reg_cmm: " + hex(write(reg_continuous_measurement_mode, 0x00) & 0x80))
-
-print("Setting cycle count")
-cs(True)
-spi.xfer2([0x04, 0x00, 0xc8, 0x00, 0xc8, 0x00, 0xc8])
-#spi.xfer2([0x04, 0x01, 0x90, 0x01, 0x90, 0x01, 0x90])
-cs(False)
-
-print("Setting up continuous measurement")
-print("reg_tmrc: " + hex(write(reg_tmrc, 0x98) & 0x80))
-print("reg_cmm: " + hex(write(reg_continuous_measurement_mode, 0x71) & 0x80))
-
-print()
-print("compass: starting measurement")
+c = Compass()
 
 while True:
-  while GPIO.input(23) == 0:
-    time.sleep(0.02)
+  c.wait_for_data()
 
-  x, y, z = get_compass_results()
-  compass_angle = calculate_compass_azimuth(x, y, z)
-
+  compass_angle = c.get_azimuth()
   update_aircraft_display(compass_angle)
