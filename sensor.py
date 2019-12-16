4d47e3508b9bc37a3976d41b826ccdb99ef908ee
Patrick Coleman <blinken@gmail.com>
2019-12-16 19:41:07 +1100

    compass, improve class and refactor pixels into class

--- sensor.py
+++ sensor.py
@@ -29,54 +29,65 @@ class Compass:
   _REG_CYCLE_COUNT_X_H = 0x04
   _REG_CYCLE_COUNT_X_L = 0x05
   _REG_TMRC = 0x0b
+  _REG_RESULT = 0x24
+
+  _READ_OFFSET = 0x80
+  _INSTRUCTION_SLEEP = 0.03
+
+  _RES_DRDY = 0x80
+
+  _GPIO_CHIP_SELECT = 24
+  _GPIO_DRDY = 23
+  _SPI_FREQ = 1000000 # 1Mhz
 
   def __init__(self):
-    GPIO.setup(24, GPIO.OUT) # Compass chip select
-    GPIO.setup(23, GPIO.IN)  # Compass DRDY
-    GPIO.output(24, GPIO.HIGH)
+    GPIO.setup(self._GPIO_CHIP_SELECT, GPIO.OUT)
+    GPIO.output(self._GPIO_CHIP_SELECT, GPIO.HIGH)
+
+    GPIO.setup(self._GPIO_DRDY, GPIO.IN)
 
     self.spi = SpiDev()
     self.spi.open(0, 0)
-    self.spi.max_speed_hz = 1000000
+    self.spi.max_speed_hz = self._SPI_FREQ
     self.spi.cshigh = False
 
     # Clear REG_POLL & reg_cmm
-    _write(_REG_POLL, 0x00)
-    _write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x00)
+    self._write(_REG_POLL, 0x00)
+    self._write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x00)
 
     # Set cycle count
-    _set_cycle_count()
+    self._set_cycle_count()
 
     # Activate continuous measurement
-    _write(_REG_TMRC, 0x98)
-    _write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71)
+    self._write(_REG_TMRC, 0x98)
+    self._write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71)
 
   def _debug(self):
-    print("Data ready: " + str(GPIO.input(23)))
+    print("Data ready: " + str(GPIO.input(self._GPIO_DRDY)))
     print("Clearing _REG_POLL & reg_cmm")
-    print("_REG_POLL: " + hex(_write(_REG_POLL, 0x00) & 0x80))
-    print("reg_cmm: " + hex(_write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x00) & 0x80))
+    print("_REG_POLL: " + hex(self._write(self._REG_POLL, 0x00) & _RES_DRDY))
+    print("reg_cmm: " + hex(self._write(self._REG_CONTINUOUS_MEASUREMENT_MODE, 0x00) & _RES_DRDY))
 
     print("Setting cycle count")
-    _set_cycle_count()
+    _self.set_cycle_count()
 
     print("Setting up continuous measurement")
-    print("_REG_TMRC: " + hex(_write(_REG_TMRC, 0x98) & 0x80))
-    print("reg_cmm: " + hex(_write(_REG_CONTINUOUS_MEASUREMENT_MODE, 0x71) & 0x80))
+    print("_REG_TMRC: " + hex(self._write(self._REG_TMRC, 0x98) & _RES_DRDY))
+    print("reg_cmm: " + hex(self._write(self._REG_CONTINUOUS_MEASUREMENT_MODE, 0x71) & _RES_DRDY))
 
   # Wrap a method that does something with the chip, activating the chip-select
   # before and after it is accessed.
   def chip_select(self, function):
     def decorator():
-      time.sleep(0.03)
-      GPIO.output(24, GPIO.LOW)
-      time.sleep(0.03)
+      time.sleep(_INSTRUCTION_SLEEP)
+      GPIO.output(self._GPIO_CHIP_SELECT, GPIO.LOW)
+      time.sleep(_INSTRUCTION_SLEEP)
 
-      result = funtion()
+      result = function()
 
-      time.sleep(0.03)
-      GPIO.output(24, GPIO.HIGH)
-      time.sleep(0.03)
+      time.sleep(_INSTRUCTION_SLEEP)
+      GPIO.output(self._GPIO_CHIP_SELECT, GPIO.HIGH)
+      time.sleep(_INSTRUCTION_SLEEP)
 
       return result
 
@@ -88,16 +99,22 @@ class Compass:
 
   @chip_select
   def _read(self, reg):
-    return self.spi.xfer2([reg | 0x80, 0x00])[1]
+    return self.spi.xfer2([reg | _READ_OFFSET, 0x00])[1]
 
   @chip_select
   def _set_cycle_count(self):
-    self.spi.xfer2([_REG_CYCLE_COUNT_X_H, 0x00, 0xc8, 0x00, 0xc8, 0x00, 0xc8])
-    #self.spi.xfer2([_REG_CYCLE_COUNT_X_H, 0x01, 0x90, 0x01, 0x90, 0x01, 0x90])
+    cycle_count = 0xc8
+    # Set the cycle count the same for x/y/z
+    self.spi.xfer2([
+      self._REG_CYCLE_COUNT_X_H,
+      0x00, cycle_count,
+      0x00, cycle_count,
+      0x00, cycle_count,
+    ])
 
   def _unpack_measurement(self, l):
     x = bytearray(l + [0x00])
-    return round((struct.unpack(">i", x)[0] >>1)/46603.0, 2)
+    return (struct.unpack(">i", x)[0] >>1)/46603.0
 
   @chip_select
   def get_raw_measurements(self):
@@ -107,27 +124,76 @@ class Compass:
     # measurement would be clocked out, then the 3 bytes for the Y axis measurement, then the 3
     # bytes for the Z axis measurement. After these 9 bytes have been clocked out, the subsequent
     # output data has no relevance.
-    self.spi.xfer2([0xa4])[0]
+
+    self.spi.xfer2([self._REG_RESULT | self._READ_OFFSET])
     results = self.spi.readbytes(9)
-    return (_unpack_measurement(results[:3]), _unpack_measurement(results[3:6]), _unpack_measurement(results[6:9]))
+
+    x = self._unpack_measurement(results[:3])
+    y = self._unpack_measurement(results[3:6])
+    z = self._unpack_measurement(results[6:9]))
+
+    return (x, y, z)
 
   def get_azimuth(self):
-    x, y, z = get_raw_measurements()
-    return round((180/math.pi * math.atan2(y, x)) % 360, 1)
+    x, y, z = self.get_raw_measurements()
+    return (180/math.pi * math.atan2(y, x)) % 360
 
   def wait_for_data(self):
-    while GPIO.input(23) == 0:
-      time.sleep(0.02)
+    while GPIO.input(self._GPIO_DRDY) == 0:
+      time.sleep(_INSTRUCTION_SLEEP) # todo, convert to wait_for_edge
 
   def to_string(self):
-    return str(get_azimuth())
+    return str(round(self.get_azimuth(), 1))
 
-pixels = neopixel.NeoPixel(board.D18, 24, auto_write=False)
+class Display:
+  _GPIO_DATA = board.D18
+  _PIXEL_COUNT = 24
+  _PIXEL_ANGLE_OFFSET = 9
 
-for i in range(24):
-  pixels[i] = (0, 0, 0)
+  _DEGREES_PER_PIXEL = 360.0/_PIXEL_COUNT
 
-pixels.show()
+  _COLOUR_COMPASS_NORTH = (0, 0, 50) # blue
+
+  def __init__(self):
+    self.pixels = neopixel.NeoPixel(board.D18, _PIXEL_COUNT, auto_write=False)
+    self.off()
+    self.pixels.show()
+
+  def off(self):
+    for i in range(self._PIXEL_COUNT):
+      self.pixels[i] = (0, 0, 0)
+
+  def calculate_pixel(self, bearing):
+    uncorrected_pixel = (self._PIXEL_COUNT - 1) - int(bearing/self._DEGREES_PER_PIXEL)
+
+    return (uncorrected_pixel + self.PIXEL_ANGLE_OFFSET) % (self._PIXEL_COUNT - 1)
+
+  def update_aircraft_display(self, compass_angle):
+    self.off()
+
+    # Show compass north in blue
+    self.pixels[calculate_pixel(compass_angle)] = _COLOUR_COMPASS_NORTH
+
+    for value in list(aircraft_list.values()):
+      ac_bearing, ac_distance = calculate_bearing(value, compass_angle)
+      if ac_distance > 100000.0:
+        # squelch aircraft too distant
+        continue
+
+      next_pixel = calculate_pixel(ac_bearing)
+      next_brightness = int(max(0, min(255, ((15000-ac_distance)*255/10000.0))))
+
+      self.pixels[next_pixel] = (next_brightness, 10, 00)
+
+    self.pixels.show()
+
+  def calculate_bearing(ac_value, compass_angle):
+    current_lat, current_lon = (0.0, 0.0) # placeholder, needs to be current location
+
+    result = Geodesic.WGS84.Inverse(current_lat, current_lon, ac_value[1], ac_value[2])
+    bearing = ((result['azi1']+180) + compass_angle) % 360
+    return (bearing, result['s12'])
+
 
 aircraft_list = {}
 
@@ -168,47 +234,10 @@ t_ac = threading.Thread(target=track_aircraft, args=(aircraft_list, ), daemon=Tr
 t_ac.start()
 
 
-def calculate_pixel(bearing):
-  degrees_per_pixel = 360/24.0
-  display_offset = 9
-  max_pixel = 23
-
-  uncorrected_pixel = max_pixel - int(bearing/degrees_per_pixel)
-
-  return (uncorrected_pixel + display_offset) % max_pixel
-
-def update_aircraft_display(compass_angle):
-  for i in range(24):
-    pixels[i] = (0, 0, 0)
-
-  # Show compass north in blue
-  pixels[calculate_pixel(compass_angle)] = (0, 0, 50)
-
-  for value in list(aircraft_list.values()):
-    ac_bearing, ac_distance = calculate_bearing(value, compass_angle)
-    if ac_distance > 100000.0:
-      # squelch aircraft too distant
-      continue
-
-    next_pixel = calculate_pixel(ac_bearing)
-    next_brightness = int(max(0, min(255, ((15000-ac_distance)*255/10000.0))))
-
-    pixels[next_pixel] = (next_brightness, 10, 00)
-
-
-  pixels.show()
-
-def calculate_bearing(ac_value, compass_angle):
-  current_lat, current_lon = (0.0, 0.0) # placeholder, needs to be current location
-
-  result = Geodesic.WGS84.Inverse(current_lat, current_lon, ac_value[1], ac_value[2])
-  bearing = ((result['azi1']+180) + compass_angle) % 360
-  return (bearing, result['s12'])
-
 c = Compass()
 
 while True:
   c.wait_for_data()
 
-  compass_angle = c.get_azimuth()
+  compass_angle = round(c.get_azimuth(), 1)
   update_aircraft_display(compass_angle)
