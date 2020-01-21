b35996799dc85d1dd93d9432110ecf6479a970bd
Patrick Coleman <blinken@gmail.com>
2020-01-21 23:20:43 +0000

    gps, test and add error handling
    serial is very, very noisy. Deal with the effects of this and attempt to handle common serial corruption and errors.

--- sensor.py
+++ sensor.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
-from spidev import SpiDev
-import time
+import io
 import struct
 import math
+import pynmea2
+import serial
 import signal
+import socket
 import sys
 import threading
-import socket
-import serial
-import pynmea2
+import time
+import traceback
 import RPi.GPIO as GPIO
-from geographiclib.geodesic import Geodesic
+
 from datetime import datetime, timedelta
+from geographiclib.geodesic import Geodesic
+from spidev import SpiDev
 
 import board
 import neopixel
@@ -157,26 +160,60 @@ class Compass:
     return str(round(self._azimuth, 1))
 
 class GPS:
+  DEVICE = "/dev/ttyAMA0"
+  SPEED = 9600
+  TIMEOUT = 3.0
+
   def __init__(self):
-    self.port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+    self._raw_port = serial.Serial(self.DEVICE, baudrate=self.SPEED, timeout=self.TIMEOUT)
+    self.port = io.TextIOWrapper(io.BufferedRWPair(self._raw_port, self._raw_port), encoding='ascii')
+
     self.latitude = 0.0
     self.longitude = 0.0
     self.updated = datetime(1970,1,1)
 
-  def track_gps(self):
-    streamreader = pynmea2.NMEAStreamReader(self.port)
+  def __del__(self):
+    self._raw_port.close()
 
+  def track_gps(self):
     while True:
-      for msg in streamreader.next():
-        try:
+      try:
+        line = self.port.readline().strip()
+        if not line or line[0] != '$':
+          # Serial can be very noisy. Silently ignore non-ASCII characters with
+          # UnicodeDecodeError below
+          continue
+
+        msg = pynmea2.parse(line)
+        if msg.sentence_type == 'GGA':
           self.latitude = msg.latitude
           self.longitude = msg.longitude
-          self.updated = msg.timestamp
-        except Exception as e:
-          print("Exception parsing NMEA - " + str(e))
+        elif msg.sentence_type == 'RMC':
+          self.updated = msg.datetime
+        else:
+          continue
+
+      except UnicodeDecodeError:
+        pass
+      except serial.SerialException as e:
+        print("Serial exception (" + str(e) + "), resetting port")
+
+        if self._raw_port:
+          self._raw_port.close()
+          self._raw_port = serial.Serial(self.DEVICE, baudrate=self.SPEED, timeout=self.TIMEOUT)
+          self.port = io.TextIOWrapper(io.BufferedRWPair(self._raw_port, self._raw_port), encoding='ascii')
+      except pynmea2.nmea.ChecksumError:
+        pass
+      except pynmea2.nmea.ParseError:
+        print("GPS: unparseable string '" + line + "'")
+      except Exception as e:
+        print("Unknown exception parsing NMEA - " + str(e))
+        traceback.print_exc()
+        time.sleep(0.2)
 
   def is_fresh(self):
-    return (datetime.now() - self.updated) < timedelta(minutes=2)
+    print("GPS fix is " + str(datetime.today() - self.updated) + " old")
+    return (datetime.today() - self.updated) < timedelta(minutes=2)
 
 class Display:
   _GPIO_DATA = board.D18
