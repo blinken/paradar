d055835a03c4552db760a0aa2ebe0af63450d670
Patrick Coleman <blinken@gmail.com>
2020-01-26 19:58:30 +0000

    board v1.1 as-built.  move v1.0 gerbers elsewhere

--- plane-spotter.cmd
+++ plane-spotter.cmd
@@ -1,5 +1,317 @@
 # Pin name action command file
 
+# Start of element C9
+ChangePinName(C9, 2, -)
+ChangePinName(C9, 1, +)
+
+# Start of element C8
+ChangePinName(C8, 2, -)
+ChangePinName(C8, 1, +)
+
+# Start of element C7
+ChangePinName(C7, 2, -)
+ChangePinName(C7, 1, +)
+
+# Start of element C6
+ChangePinName(C6, 2, -)
+ChangePinName(C6, 1, +)
+
+# Start of element C5
+ChangePinName(C5, 2, -)
+ChangePinName(C5, 1, +)
+
+# Start of element C4
+ChangePinName(C4, 2, -)
+ChangePinName(C4, 1, +)
+
+# Start of element PX36
+ChangePinName(PX36, 6, 6)
+ChangePinName(PX36, 5, 5)
+ChangePinName(PX36, 4, 4)
+ChangePinName(PX36, 3, 3)
+ChangePinName(PX36, 2, 2)
+ChangePinName(PX36, 1, 1)
+
+# Start of element PX35
+ChangePinName(PX35, 6, 6)
+ChangePinName(PX35, 5, 5)
+ChangePinName(PX35, 4, 4)
+ChangePinName(PX35, 3, 3)
+ChangePinName(PX35, 2, 2)
+ChangePinName(PX35, 1, 1)
+
+# Start of element PX34
+ChangePinName(PX34, 6, 6)
+ChangePinName(PX34, 5, 5)
+ChangePinName(PX34, 4, 4)
+ChangePinName(PX34, 3, 3)
+ChangePinName(PX34, 2, 2)
+ChangePinName(PX34, 1, 1)
+
+# Start of element PX33
+ChangePinName(PX33, 6, 6)
+ChangePinName(PX33, 5, 5)
+ChangePinName(PX33, 4, 4)
+ChangePinName(PX33, 3, 3)
+ChangePinName(PX33, 2, 2)
+ChangePinName(PX33, 1, 1)
+
+# Start of element PX32
+ChangePinName(PX32, 6, 6)
+ChangePinName(PX32, 5, 5)
+ChangePinName(PX32, 4, 4)
+ChangePinName(PX32, 3, 3)
+ChangePinName(PX32, 2, 2)
+ChangePinName(PX32, 1, 1)
+
+# Start of element PX31
+ChangePinName(PX31, 6, 6)
+ChangePinName(PX31, 5, 5)
+ChangePinName(PX31, 4, 4)
+ChangePinName(PX31, 3, 3)
+ChangePinName(PX31, 2, 2)
+ChangePinName(PX31, 1, 1)
+
+# Start of element PX30
+ChangePinName(PX30, 6, 6)
+ChangePinName(PX30, 5, 5)
+ChangePinName(PX30, 4, 4)
+ChangePinName(PX30, 3, 3)
+ChangePinName(PX30, 2, 2)
+ChangePinName(PX30, 1, 1)
+
+# Start of element PX29
+ChangePinName(PX29, 6, 6)
+ChangePinName(PX29, 5, 5)
+ChangePinName(PX29, 4, 4)
+ChangePinName(PX29, 3, 3)
+ChangePinName(PX29, 2, 2)
+ChangePinName(PX29, 1, 1)
+
+# Start of element PX28
+ChangePinName(PX28, 6, 6)
+ChangePinName(PX28, 5, 5)
+ChangePinName(PX28, 4, 4)
+ChangePinName(PX28, 3, 3)
+ChangePinName(PX28, 2, 2)
+ChangePinName(PX28, 1, 1)
+
+# Start of element PX27
+ChangePinName(PX27, 6, 6)
+ChangePinName(PX27, 5, 5)
+ChangePinName(PX27, 4, 4)
+ChangePinName(PX27, 3, 3)
+ChangePinName(PX27, 2, 2)
+ChangePinName(PX27, 1, 1)
+
+# Start of element PX26
+ChangePinName(PX26, 6, 6)
+ChangePinName(PX26, 5, 5)
+ChangePinName(PX26, 4, 4)
+ChangePinName(PX26, 3, 3)
+ChangePinName(PX26, 2, 2)
+ChangePinName(PX26, 1, 1)
+
+# Start of element PX25
+ChangePinName(PX25, 6, 6)
+ChangePinName(PX25, 5, 5)
+ChangePinName(PX25, 4, 4)
+ChangePinName(PX25, 3, 3)
+ChangePinName(PX25, 2, 2)
+ChangePinName(PX25, 1, 1)
+
+# Start of element PX24
+ChangePinName(PX24, 6, 6)
+ChangePinName(PX24, 5, 5)
+ChangePinName(PX24, 4, 4)
+ChangePinName(PX24, 3, 3)
+ChangePinName(PX24, 2, 2)
+ChangePinName(PX24, 1, 1)
+
+# Start of element PX23
+ChangePinName(PX23, 6, 6)
+ChangePinName(PX23, 5, 5)
+ChangePinName(PX23, 4, 4)
+ChangePinName(PX23, 3, 3)
+ChangePinName(PX23, 2, 2)
+ChangePinName(PX23, 1, 1)
+
+# Start of element PX22
+ChangePinName(PX22, 6, 6)
+ChangePinName(PX22, 5, 5)
+ChangePinName(PX22, 4, 4)
+ChangePinName(PX22, 3, 3)
+ChangePinName(PX22, 2, 2)
+ChangePinName(PX22, 1, 1)
+
+# Start of element PX21
+ChangePinName(PX21, 6, 6)
+ChangePinName(PX21, 5, 5)
+ChangePinName(PX21, 4, 4)
+ChangePinName(PX21, 3, 3)
+ChangePinName(PX21, 2, 2)
+ChangePinName(PX21, 1, 1)
+
+# Start of element PX20
+ChangePinName(PX20, 6, 6)
+ChangePinName(PX20, 5, 5)
+ChangePinName(PX20, 4, 4)
+ChangePinName(PX20, 3, 3)
+ChangePinName(PX20, 2, 2)
+ChangePinName(PX20, 1, 1)
+
+# Start of element PX19
+ChangePinName(PX19, 6, 6)
+ChangePinName(PX19, 5, 5)
+ChangePinName(PX19, 4, 4)
+ChangePinName(PX19, 3, 3)
+ChangePinName(PX19, 2, 2)
+ChangePinName(PX19, 1, 1)
+
+# Start of element PX18
+ChangePinName(PX18, 6, 6)
+ChangePinName(PX18, 5, 5)
+ChangePinName(PX18, 4, 4)
+ChangePinName(PX18, 3, 3)
+ChangePinName(PX18, 2, 2)
+ChangePinName(PX18, 1, 1)
+
+# Start of element PX17
+ChangePinName(PX17, 6, 6)
+ChangePinName(PX17, 5, 5)
+ChangePinName(PX17, 4, 4)
+ChangePinName(PX17, 3, 3)
+ChangePinName(PX17, 2, 2)
+ChangePinName(PX17, 1, 1)
+
+# Start of element PX16
+ChangePinName(PX16, 6, 6)
+ChangePinName(PX16, 5, 5)
+ChangePinName(PX16, 4, 4)
+ChangePinName(PX16, 3, 3)
+ChangePinName(PX16, 2, 2)
+ChangePinName(PX16, 1, 1)
+
+# Start of element PX15
+ChangePinName(PX15, 6, 6)
+ChangePinName(PX15, 5, 5)
+ChangePinName(PX15, 4, 4)
+ChangePinName(PX15, 3, 3)
+ChangePinName(PX15, 2, 2)
+ChangePinName(PX15, 1, 1)
+
+# Start of element PX14
+ChangePinName(PX14, 6, 6)
+ChangePinName(PX14, 5, 5)
+ChangePinName(PX14, 4, 4)
+ChangePinName(PX14, 3, 3)
+ChangePinName(PX14, 2, 2)
+ChangePinName(PX14, 1, 1)
+
+# Start of element PX13
+ChangePinName(PX13, 6, 6)
+ChangePinName(PX13, 5, 5)
+ChangePinName(PX13, 4, 4)
+ChangePinName(PX13, 3, 3)
+ChangePinName(PX13, 2, 2)
+ChangePinName(PX13, 1, 1)
+
+# Start of element PX12
+ChangePinName(PX12, 6, 6)
+ChangePinName(PX12, 5, 5)
+ChangePinName(PX12, 4, 4)
+ChangePinName(PX12, 3, 3)
+ChangePinName(PX12, 2, 2)
+ChangePinName(PX12, 1, 1)
+
+# Start of element PX11
+ChangePinName(PX11, 6, 6)
+ChangePinName(PX11, 5, 5)
+ChangePinName(PX11, 4, 4)
+ChangePinName(PX11, 3, 3)
+ChangePinName(PX11, 2, 2)
+ChangePinName(PX11, 1, 1)
+
+# Start of element PX10
+ChangePinName(PX10, 6, 6)
+ChangePinName(PX10, 5, 5)
+ChangePinName(PX10, 4, 4)
+ChangePinName(PX10, 3, 3)
+ChangePinName(PX10, 2, 2)
+ChangePinName(PX10, 1, 1)
+
+# Start of element PX9
+ChangePinName(PX9, 6, 6)
+ChangePinName(PX9, 5, 5)
+ChangePinName(PX9, 4, 4)
+ChangePinName(PX9, 3, 3)
+ChangePinName(PX9, 2, 2)
+ChangePinName(PX9, 1, 1)
+
+# Start of element PX8
+ChangePinName(PX8, 6, 6)
+ChangePinName(PX8, 5, 5)
+ChangePinName(PX8, 4, 4)
+ChangePinName(PX8, 3, 3)
+ChangePinName(PX8, 2, 2)
+ChangePinName(PX8, 1, 1)
+
+# Start of element PX7
+ChangePinName(PX7, 6, 6)
+ChangePinName(PX7, 5, 5)
+ChangePinName(PX7, 4, 4)
+ChangePinName(PX7, 3, 3)
+ChangePinName(PX7, 2, 2)
+ChangePinName(PX7, 1, 1)
+
+# Start of element PX6
+ChangePinName(PX6, 6, 6)
+ChangePinName(PX6, 5, 5)
+ChangePinName(PX6, 4, 4)
+ChangePinName(PX6, 3, 3)
+ChangePinName(PX6, 2, 2)
+ChangePinName(PX6, 1, 1)
+
+# Start of element PX5
+ChangePinName(PX5, 6, 6)
+ChangePinName(PX5, 5, 5)
+ChangePinName(PX5, 4, 4)
+ChangePinName(PX5, 3, 3)
+ChangePinName(PX5, 2, 2)
+ChangePinName(PX5, 1, 1)
+
+# Start of element PX4
+ChangePinName(PX4, 6, 6)
+ChangePinName(PX4, 5, 5)
+ChangePinName(PX4, 4, 4)
+ChangePinName(PX4, 3, 3)
+ChangePinName(PX4, 2, 2)
+ChangePinName(PX4, 1, 1)
+
+# Start of element PX3
+ChangePinName(PX3, 6, 6)
+ChangePinName(PX3, 5, 5)
+ChangePinName(PX3, 4, 4)
+ChangePinName(PX3, 3, 3)
+ChangePinName(PX3, 2, 2)
+ChangePinName(PX3, 1, 1)
+
+# Start of element PX2
+ChangePinName(PX2, 6, 6)
+ChangePinName(PX2, 5, 5)
+ChangePinName(PX2, 4, 4)
+ChangePinName(PX2, 3, 3)
+ChangePinName(PX2, 2, 2)
+ChangePinName(PX2, 1, 1)
+
+# Start of element PX1
+ChangePinName(PX1, 6, 6)
+ChangePinName(PX1, 5, 5)
+ChangePinName(PX1, 4, 4)
+ChangePinName(PX1, 3, 3)
+ChangePinName(PX1, 2, 2)
+ChangePinName(PX1, 1, 1)
+
 # Start of element COMPASS1
 ChangePinName(COMPASS1, 8, 8)
 ChangePinName(COMPASS1, 7, 7)
@@ -9,14 +321,6 @@ ChangePinName(COMPASS1, 4, 4)
 ChangePinName(COMPASS1, 3, 3)
 ChangePinName(COMPASS1, 2, 2)
 ChangePinName(COMPASS1, 1, 1)
-
-# Start of element PIXEL1
-ChangePinName(PIXEL1, 6, 6)
-ChangePinName(PIXEL1, 5, 5)
-ChangePinName(PIXEL1, 4, 4)
-ChangePinName(PIXEL1, 3, 3)
-ChangePinName(PIXEL1, 2, 2)
-ChangePinName(PIXEL1, 1, 1)
 
 # Start of element C1
 ChangePinName(C1, 2, -)
