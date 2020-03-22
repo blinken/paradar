163ce6c4e47a05279d7e72685c05b3a72ba85be6
Patrick Coleman <blinken@gmail.com>
2020-03-22 12:49:59 +0000

    deploy, add script to clean up and zero filesystem

---
+++ deploy/cleanup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ `id -u` -ne 0 ]; then
+  echo "Must be run as root."
+  exit 1
+fi
+
+echo "=> Entering r/w mode"
+/bin/rw
+
+echo "=> apt dist-upgrade and clean"
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y autoremove
+apt-get clean
+
+echo "=> Updating /opt/paradar"
+cd /opt/paradar
+git pull --rebase
+
+if [ $? -ne 0 ]; then
+  echo "Rebase failed - aborting."
+  exit 1
+fi
+
+cd /home/blinken
+
+echo "=> Removing history files"
+rm -rvf {/root,/home/blinken}/{.bash_history,.viminfo,.lesshst,.ssh/known_hosts}
+
+echo "=> Emptying /storage"
+rm -rvf /storage/*
+
+echo "=> zeroing /boot"
+rm -f /boot/zerofile
+dd if=/dev/zero of=/boot/zerofile bs=1M
+rm -f /boot/zerofile
+
+echo "=> entering r/o mode"
+/bin/ro
+
+echo "=> zerofree /"
+zerofree -v /dev/mmcblk0p2
+
+echo "=> remount /storage ro"
+sudo mount -o remount,ro /dev/mmcblk0p3
+
+echo "=> zerofree /storage"
+zerofree -v /dev/mmcblk0p3
+
+echo "=> remount /storage rw"
+sudo mount -o remount,rw /dev/mmcblk0p3
+
+
